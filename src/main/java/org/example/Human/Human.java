66198c5457d4daa500e6b82d83fffef2f505f8a6
Teodor Poshyvak <teodor.poshyvak.t@gmail.com>
2023-09-16 20:22:00 +0300

    Initial commit

---
+++ src/main/java/org/example/Human/Human.java
@@ -0,0 +1,19 @@
+package org.example.Human;
+
+public class Human {
+    private String firstName;
+    private String lastName;
+
+    public Human(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
